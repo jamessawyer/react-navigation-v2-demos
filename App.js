4ebf86422081df66bbf7c50cf9a693a44d06d443
qiwentao <qiwentao@MacdeiMac.local>
2018-08-15 17:37:46 +0800

    feat: 完成bottom tab navigator demo

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -11,32 +5,70 @@ import {
   Text,
   View
 } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!That can be fun
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js how
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <Text>
+          主页
         </Text>
       </View>
     );
   }
 }
+class Settings extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          设置页面
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default createBottomTabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: '主页', // tab的标签名
+      tabBarIcon: ({ focused, tintColor }) => (
+        <Icon name='ios-home' color={tintColor} size={24} />
+      ),
+    }
+  },
+  Settings: {
+    screen: Settings,
+    navigationOptions: {
+      tabBarLabel: '设置', // tab的标签名
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name='ios-settings' color={tintColor} size={24} />
+      ),
+    }
+  },
+}, { // 路由配置
+  initialRouteName: 'Settings', // 初始tab bar 页面
+  order: ['Settings', 'Home'], // tab bar的显示顺序
+  navigationOptions: {
+    tabBarVisible: true, // 是否显示tab bar 默认是true
+  },
+  tabBarOptions: {
+    // 用来style tab bar
+    activeTintColor: 'orangered', // 激活时的样式
+    inactiveTintColor: 'grey',
+    swipeEnabled: true,
+    tabStyle: {
+      // backgroundColor: 'pink'
+    },
+    style: {
+      backgroundColor: 'blue',
+    },
+  },
+})
 
 const styles = StyleSheet.create({
   container: {
@@ -44,15 +76,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });

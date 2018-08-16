c43e3aeec5cc91bb53ad1128609024cf5dd302e1
qiwentao <qiwentao@MacdeiMac.local>
2018-08-16 10:41:25 +0800

    feat: react navigation material bottom tabs demo完成

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -11,32 +5,104 @@ import {
   Text,
   View
 } from 'react-native';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+class HomeScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!That can be fun
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js how
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <Text>
+          主页
         </Text>
       </View>
     );
   }
 }
+class SettingsScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          设置页面
+        </Text>
+      </View>
+    );
+  }
+}
+class ProfileScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          个人主页
+        </Text> 
+      </View>
+    );
+  }
+}
+class FeedScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          主页
+        </Text>
+      </View>
+    );
+  }
+}
+
+
+export default createMaterialBottomTabNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarLabel: '主页',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name='ios-home' color={tintColor} size={24} />
+      ),
+    }
+  },
+  Settings: {
+    screen: SettingsScreen,
+    navigationOptions: {
+      tabBarLabel: '设置',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name='ios-settings' color={tintColor} size={24} />
+      ),
+    }
+  },
+  Profile: {
+    screen: ProfileScreen,
+    navigationOptions: {
+      tabBarLabel: '个人中心',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name='ios-list-box' color={tintColor} size={24} />
+      ),
+    }
+  },
+  Feed: {
+    screen: FeedScreen,
+    navigationOptions: {
+      tabBarLabel: '反馈',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name='ios-microphone' color={tintColor} size={24} />
+      ),
+    }
+  },
+}, {
+  initialRouteName: 'Settings',
+  // order: ['Settings', 'Home'],
+  activeTintColor: 'black',
+  inactiveTintColor: '#333',
+  // 缩放图标的效果
+  shifting: true, // 默认在大于3个路由时为true, 如果显式的设置为true了则少于3个时也会显示效果
+  barStyle: {
+    backgroundColor: 'orange',
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -44,15 +110,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
